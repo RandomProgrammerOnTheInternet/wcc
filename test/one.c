336b5c3504a5b5e30ee606b6d8f47918fa3eaa06
therealblue24 <99767624+therealblue24@users.noreply.github.com>
2026-06-20 16:01:16 -0400

    oops

--- test/one.c
+++ test/one.c
@@ -3,5 +3,6 @@ long two(long x);
 long argument_waster(long a, long b, long c, long d, long e, long f, long g,
 					 long h, long i, long j, long k, long l);
 void putchar(int c);
+void puts(char *s);
 void print_num_helper(int num);
 void print_num(int num);
